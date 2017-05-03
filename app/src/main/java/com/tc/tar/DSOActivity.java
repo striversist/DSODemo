7ac02d45218313bf2b7af9a8a4a10d5556389baa
aarontang <aarontang@tencent.com>
2017-05-03 10:36:09 +0800

    [env]init empty project

---
+++ app/src/main/java/com/tc/tar/DSOActivity.java
@@ -0,0 +1,13 @@
+package com.tc.tar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DSOActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dso);
+    }
+}
